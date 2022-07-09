e37f8841d29bed0b43c0cec4a708a9c30b67954b
YoungKyonYou <man.spride1324@gmail.com>
2022-07-10 00:48:28 +0900

    inital commit

---
+++ src/test/java/java_stream_study/JavaStreamStudyApplicationTests.java
@@ -0,0 +1,32 @@
+package java_stream_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@SpringBootTest
+class JavaStreamStudyApplicationTests {
+
+    @Test
+    void StreamSetAdd() {
+        List<Integer>list1=Arrays.asList(1,2,3);
+        List<Integer>list2=Arrays.asList(4,5,6);
+        List<Integer>list3=Arrays.asList(7,8,9);
+        List<Integer> list4= Arrays.asList(10,0,-1);
+
+        Set<Integer> set= Stream.of(list1, list2, list3, list4)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet());
+        
+        for(Integer s: set){
+            System.out.println("s = " + s);
+        }
+    }
+
+}
